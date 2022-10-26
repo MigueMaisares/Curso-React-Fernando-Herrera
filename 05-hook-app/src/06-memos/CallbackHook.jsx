426df25192e7706ff7ac6ccdeec8b9806b8203fb
MigueMaisares <miguemaisares@gmail.com>
2022-10-26 00:18:18 -0300

    5to push

--- 05-hook-app/src/06-memos/CallbackHook.jsx
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
+  //Las funciones y los objetos en JS siempre apuntan a posiciones de memoria diferente
+  //Cada vez que el comp CallbackHook se renderize, enviara distintos incrementos entre si
   const [counter, setCounter] = useState(10);
-  const incremento = () => {
+
+  //Memoriza funciones y devuelve otra funcion memorizada en el mismo espacio de memoria
+  const increment = useCallback(
+    //esta callback tiene que ser la misma que se manda a llamar, es como su definicion dentro de este hook
+    (value) => {
+      //esta instruccion es explicita, si fuerea setCounter(counter + 1) quedaria siempre en el mismo valor
+      setCounter((counter) => counter + value);
+    },
+    []
+  );
+
+  /* const incremento = () => {
     setCounter(counter + 1);
-  };
+  }; */
   return (
     <>
       <h1>useCallback hook: {counter}</h1>
       <hr />
 
-      <ShowIncrement increment={incremento} />
+      <ShowIncrement increment={increment} />
     </>
   );
 };
--- 05-hook-app/src/06-memos/ShowIncrement.jsx
+++ 05-hook-app/src/06-memos/ShowIncrement.jsx
@@ -1,9 +1,11 @@
-export const ShowIncrement = ({ increment }) => {
-  console.log("Me volvi a generar :c");
+import React from "react";
 
+//      Si aca uso React.memo() en este componente ShowIncrement, no podra memorizarlo porque las funciones cada vez que se renderizan, apuntan a espacios de memoria distintas
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log("Me volvi a generar :c");
   return (
-    <button className="btn btn-primary" onClick={increment}>
+    <button className="btn btn-primary" onClick={() => increment(5)}>
       Incrementar
     </button>
   );
-};
+});
